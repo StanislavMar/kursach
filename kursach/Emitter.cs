32f626a8104c29197ecfd9e43dbd5df6e4dde3c4
Мараховский Стас <stas20033@list.ru>
2021-12-24 10:52:35 +0800

    Сделал затухание частиц, следование за мышью

--- kursach/Emitter.cs
+++ kursach/Emitter.cs
@@ -13,7 +13,7 @@ namespace kursach
         public List<Particle> particles = new List<Particle>();
 
         Random rand = new Random();
-        public int zxc;
+        public int zx;
         public int MousePositionX = 0;
         public int MousePositionY = 0;
         public float GravitationX = 0;
